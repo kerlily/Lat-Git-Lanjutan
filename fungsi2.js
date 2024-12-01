0c381557e1c5d8affb9089863a5b44eee0f83ff7
kerlily <farhan15062004@gmail.com>
2024-12-01 17:37:00 +0700

    menghapus data ga perlu

--- fungsi2.js
+++ fungsi2.js
@@ -1,5 +1,5 @@
-console.log("hello world");
-console.log("hello dunia");
-console.log("hello indonesia");
 const add = (a,b) => a + b;
 console.log(add(1,2));
+
+const substract = (a,b) => a - b;
+console.log(substract(10,2));
--- test.js
+++
@@ -1,2 +0,0 @@
-console.log("apalah");
-console.log("apalah2");
